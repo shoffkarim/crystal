3f7e36e5ea3f77bd0033abf67087cbf029687460
shoffkarim <k4r1mch1k@gmail.com>
2020-10-26 16:04:32 +0500

    адаптив

--- app/build/js/s.js
+++ app/build/js/s.js
@@ -14,3 +14,13 @@ if (document.documentElement.clientWidth < 1550) {
   textImgThird.setAttribute('y', "290");
   console.log(textImgFirst.y);
 }
+
+if (document.documentElement.clientWidth < 767) {
+  let textImgFirst = document.querySelector('.text-img.first');
+  let textImgSecond = document.querySelector('.text-img.second');
+  let textImgThird = document.querySelector('.text-img.third');
+  textImgFirst.setAttribute('x', "25");
+  textImgSecond.setAttribute('x', "25");
+  textImgThird.setAttribute('x', "25");
+  console.log(textImgFirst.y);
+}
